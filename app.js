49a40b41d4714b50a8a160128573257f44c83701
paaavleee <kurtanidzepavle@gmail.com>
2022-10-23 15:50:29 +0400

    fin

---
+++ app.js
@@ -0,0 +1,44 @@
+let myName = "pavle";
+
+
+let myHobby = "football";
+
+let yearsOfBerth = 1989;
+let currentYear = 2022;
+let month = "september";
+let date = 10;
+let city = "tbilisi"
+
+console.log (typeof myName, );
+console.log (typeof yearsOfBerth, currentYear)
+
+let myage = currentYear - yearsOfBerth;
+
+
+let myAge = currentYear - yearsOfBerth;
+
+console.log(myAge)
+
+let text1 = "my name is" + myName;
+
+let text2 = `my name is ${myName}`;
+
+console.log(text1, text2)
+
+let text3 = " i was born in "  + yearsOfBerth  +" "+  date  + month;
+let text4 = `i was born in ${yearsOfBerth} ${date} ${month}`;
+console.log(text3, text4)
+
+let text5 = "my hobby is " + myHobby;
+
+console.log (text5)
+
+let aboutMe = text2+"." + text3+"." +" i live in "+ city;
+
+console.log(aboutMe)
+
+
+
+
+
+
